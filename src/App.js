4ec99b9a3bb76875b4e218be5a82ea5bfd7e4f46
William Salas <willbullet52@gmail.com>
2023-03-24 14:41:07 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -2,85 +2,93 @@ import "./styles.css";
 import { v4 as uuidv4 } from "uuid";
 import { useState, useRef, useEffect } from "react";
 import { supabase } from "./config/supabase";
+import { Heading, Spinner } from "@chakra-ui/react";
 
 export default function App() {
-  //state
-  const [file, setFile] = useState({});
-  const [images, setImages] = useState([]);
-  const inputRef = useRef(null);
-  const fetched = useRef(false);
-  const BucketURL =
-    "https://zjfiouexnmcnwltbzgyi.supabase.co/storage/v1/object/public/image-bucket/";
-  //hanlders
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const filename = `${uuidv4()}-${file.name}`;
+	//state
+	const [file, setFile] = useState({});
+	const [loading, setLoading] = useState(true);
+	const [images, setImages] = useState([]);
+	const inputRef = useRef(null);
+	const fetched = useRef(false);
+	const BucketURL =
+		"https://zjfiouexnmcnwltbzgyi.supabase.co/storage/v1/object/public/image-bucket/";
+	//hanlders
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const filename = `${uuidv4()}-${file.name}`;
 
-    const { data, error } = await supabase.storage
-      .from("image-bucket")
-      .upload(filename, file, {
-        cacheControl: "3600",
-        upsert: false
-      });
-    inputRef.current.value = "";
-    fetchAllFiles();
-    if (data) console.log(data);
-    else {
-      console.log(error);
-    }
-  };
-  const handleChange = (e) => {
-    setFile(e.target.files[0]);
-  };
+		const { data, error } = await supabase.storage
+			.from("image-bucket")
+			.upload(filename, file, {
+				cacheControl: "3600",
+				upsert: false,
+			});
+		inputRef.current.value = "";
+		fetchAllFiles();
+		if (data) console.log(data);
+		else {
+			console.log(error);
+		}
+	};
+	const handleChange = (e) => {
+		setFile(e.target.files[0]);
+	};
+	//fetches all files
+	const fetchAllFiles = async () => {
+		const { data: files, error } = await supabase.storage
+			.from("image-bucket")
+			.list("");
+		if (error) {
+			console.error(error);
+			return;
+		}
+		// Do something with the list of files
+		setImages(files);
+	};
 
-  const fetchAllFiles = async () => {
-    const { data: files, error } = await supabase.storage
-      .from("image-bucket")
-      .list("");
-    if (error) {
-      console.error(error);
-      return;
-    }
+	useEffect(() => {
+		if (fetched.current) {
+			return;
+		}
+		setTimeout(() => {
+			setLoading(false);
+		}, 1500);
+		fetched.current = true;
+		fetchAllFiles();
+		console.log("data fetched! initial fetch");
+	}, []);
 
-    // Do something with the list of files
-    setImages(files);
-  };
-
-  useEffect(() => {
-    if (fetched.current) {
-      return;
-    }
-    fetched.current = true;
-    fetchAllFiles();
-    console.log("data fetched! initial fetch");
-  }, []);
-
-  return (
-    <div className="App">
-      <form className="imageForm" onSubmit={handleSubmit}>
-        <label>Upload your image!</label>
-        <input
-          ref={inputRef}
-          name="image"
-          onChange={handleChange}
-          type="file"
-        />
-        <input type="submit" value="Upload" />
-      </form>
-      <div className="imageDiv">
-        {images
-          ? images.map((i) => {
-              return (
-                <img
-                  className="imageRendered"
-                  key={i.id}
-                  src={BucketURL + i.name}
-                  alt="supabase"
-                />
-              );
-            })
-          : null}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<form className="imageForm" onSubmit={handleSubmit}>
+				<Heading>{"Upload your image!"}</Heading>
+				<input
+					ref={inputRef}
+					name="image"
+					onChange={handleChange}
+					type="file"
+				/>
+				<button className="upload" type="submit">
+					Upload
+				</button>
+			</form>
+			<div className="imageDiv">
+				{loading ? (
+					<Spinner size="xl" color="blue.500" />
+				) : (
+					images.map((i) => {
+						return (
+							<img
+								className="imageRendered"
+								key={i.id}
+								src={BucketURL + i.name}
+								alt="supabase"
+							/>
+						);
+					})
+				)}
+			</div>
+		</div>
+	);
 }
--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://zjfiouexnmcnwltbzgyi.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqZmlvdWV4bm1jbndsdGJ6Z3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk2MTIxMTYsImV4cCI6MTk5NTE4ODExNn0.agdGMqcyWMoZ9n7dYUmoOP2-BNGGd9CWxuiP0d4XvVg"
+	process.env.REACT_APP_SUPABASE_URL,
+	process.env.REACT_APP_SUPABASE_ANON_KEY
 );
